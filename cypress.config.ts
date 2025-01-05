4382e2de74c0e067b1f036110237a66a2949be2d
oshigoto46 <shirou.tab@gmail.com>
2025-01-05 15:22:03 +0900

    Removed all files except .git

--- cypress.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "cypress";
-
-export default defineConfig({
-  e2e: {
-    specPattern: "cypress/e2e/**/*.ts", // テストファイルの検索パターン
-    baseUrl: "http://localhost:3000",   // 必要に応じて
-  },
-});
